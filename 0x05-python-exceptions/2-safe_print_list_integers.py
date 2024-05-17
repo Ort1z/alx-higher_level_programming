f8e08a1a8eb0bad84dc5c2f607494cd7ff397b6d
MASINGITA OTTIS MALULEKE <masingitaottismaluleke@gmail.com>
2024-05-16 18:31:18 -0700

    Adding Scripts and files in 0x05-python-exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    idx = 0
+    count = 0
+    while my_list and idx < x:
+        try:
+            print("{:d}".format(my_list[idx]), end="")
+            idx += 1
+            count += 1
+        except ValueError:
+            idx += 1
+            continue
+        except TypeError:
+            idx += 1
+            continue
+    print()
+    return count
